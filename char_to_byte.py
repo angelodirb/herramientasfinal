d92f8d9a50089087b827e76d8fa0fd25ee9476bb
angelodirb <168436547+angelodirb@users.noreply.github.com>
2024-05-23 21:58:18 -0500

    Revert "Implementar función char_to_byte"

--- char_to_byte.py
+++
@@ -1,7 +0,0 @@
-def char_to_byte(char):
-    """Convierte un carácter a su representación en byte (binario)."""
-    return format(ord(char), '08b')
-
-if __name__ == "__main__":
-    char = input("Ingrese un carácter valido para poder realizar el procedimiento : ")
-    print(f"La representación en byte de '{char}' es: {char_to_byte(char)}")
